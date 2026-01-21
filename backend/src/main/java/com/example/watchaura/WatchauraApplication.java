767fc136ff0e538417cba63c73fab581fbb5c899
TuanTran1106 <tuantaph50121@gmail.com>
2026-01-21 22:17:29 +0700

    Init Spring Boot project WatchAura

---
+++ backend/src/main/java/com/example/watchaura/WatchauraApplication.java
@@ -0,0 +1,13 @@
+package com.example.watchaura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchauraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatchauraApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/watchaura/WatchauraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.watchaura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchauraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
